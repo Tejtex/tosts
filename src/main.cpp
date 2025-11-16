5071ecd25fa43a09dd1a5382bf06e2fadfa6e2cc
Tejtex <tejtex.dev@gmail.com>
2025-11-16 20:03:38 +0100

    added process.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,52 +2,77 @@
 #include <string>
 #include <vector>
 #include "load_tests.cpp"
+#include "process.cpp"
 
-struct Args {
+struct Args
+{
     std::string in, out, cmd;
 };
 
-bool parse(int argc, char* argv[], Args& a) {
+bool parse(int argc, char *argv[], Args &a)
+{
     std::vector<std::string> v(argv + 1, argv + argc);
     bool has_i = false, has_o = false;
     size_t i = 0;
-    while (i < v.size()) {
-        if (v[i] == "-i") {
-            if (i + 1 >= v.size()) return std::cerr << "No input directory given after -i\n", false;
-            if (has_i) return std::cerr << "2 -i flags\n", false;
-            has_i = true; a.in = v[++i];
+    while (i < v.size())
+    {
+        if (v[i] == "-i")
+        {
+            if (i + 1 >= v.size())
+                return std::cerr << "No input directory given after -i\n", false;
+            if (has_i)
+                return std::cerr << "2 -i flags\n", false;
+            has_i = true;
+            a.in = v[++i];
         }
-        else if (v[i] == "-o") {
-            if (i + 1 >= v.size()) return std::cerr << "No output directory given after -o\n", false;
-            if (has_o) return std::cerr << "2 -o flags\n", false;
-            has_o = true; a.out = v[++i];
+        else if (v[i] == "-o")
+        {
+            if (i + 1 >= v.size())
+                return std::cerr << "No output directory given after -o\n", false;
+            if (has_o)
+                return std::cerr << "2 -o flags\n", false;
+            has_o = true;
+            a.out = v[++i];
         }
-        else if (v[i][0] != '-') break;
-        else return std::cerr << "Unknown command: " + v[i] + "\n", false;
+        else if (v[i][0] != '-')
+            break;
+        else
+            return std::cerr << "Unknown command: " + v[i] + "\n", false;
         ++i;
     }
     a.cmd.clear();
-    for (; i < v.size(); ++i) {
-        if (!a.cmd.empty()) a.cmd += " ";
+    for (; i < v.size(); ++i)
+    {
+        if (!a.cmd.empty())
+            a.cmd += " ";
         a.cmd += v[i];
     }
 
-    if(!has_i) return std::cerr << "No input directory(-i)\n", false;
-    if(!has_o) return std::cerr << "No output directory(-o)\n", false;
-    if(a.cmd.empty()) return std::cerr << "No command to run your program\n", false;
+    if (!has_i)
+        return std::cerr << "No input directory(-i)\n", false;
+    if (!has_o)
+        return std::cerr << "No output directory(-o)\n", false;
+    if (a.cmd.empty())
+        return std::cerr << "No command to run your program\n", false;
     return true;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     Args a;
-    if (!parse(argc, argv, a)) return 1;
+    if (!parse(argc, argv, a))
+        return 1;
 
     Stats stats;
     std::vector<std::string> tests;
 
-    if(!load_tests(a.in, a.out, stats, tests)) return 1;
-    for(auto s : tests) std::cerr << s << "\n";
-    std::cerr << "\n";
-    for(auto s : stats.skipped) std::cerr << s << "\n";
+    if (!load_tests(a.in, a.out, stats, tests))
+        return 1;
+    std::cerr << "Tests:\n";
+    for (auto s : tests)
+        std::cerr << s << "\n";
+    std::cerr << "Skipped\n";
+    for (auto s : stats.skipped)
+        std::cerr << s << "\n";
     return 0;
 }
